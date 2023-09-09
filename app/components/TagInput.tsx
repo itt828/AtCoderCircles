ac5b5f0fba05f4d33b0d8d521308f0acfc04ad0c
itt828 <iittt828@gmail.com>
2023-09-09 16:28:16 +0900

    :zap: Tag input

--- app/components/TagInput.tsx
+++ app/components/TagInput.tsx
@@ -1,32 +1,40 @@
+import { useState } from "react";
 import { useFieldArray } from "remix-validated-form";
+import Input from "./input";
+import Button from "./button";
 
 export default function TagInput({
   name,
+  formId,
   value,
 }: {
   name: string;
-  value: string[];
+  formId?: string;
+  value?: string[];
 }) {
-  const [items, { push, remove }] = useFieldArray("tags", {
-    formId: "tag-input",
-  });
+  const [items, { push, remove }] = useFieldArray(name, { formId });
+  const [newTagName, setNewTagName] = useState("");
   return (
     <>
       {items.map(({ defaultValue, key }, index) => (
-        <>
-          <div>{defaultValue}</div>
-          <div>{key}</div>
-          <button
+        <div key={key}>
+          <span>{defaultValue}</span>
+          {/* <div>{key}</div> */}
+          <Button
             onClick={() => {
               remove(index);
             }}
           >
             x
-          </button>
-        </>
+          </Button>
+        </div>
       ))}
-      <input />
-      <button onClick={() => push("")}>add</button>
+      <Input
+        name="newTag"
+        value={newTagName}
+        onChange={(e) => setNewTagName(e.target.value)}
+      />
+      <Button onClick={() => push(newTagName)}>add</Button>
     </>
   );
 }
